31e2950248eadc8d7a86299eff4f7792f50a001e
Carl <Chengchenghu0728@gmail.com>
2023-04-16 11:52:21 +0800

    Update graph.cpp

--- algorithm/graph.cpp
+++ algorithm/graph.cpp
@@ -2,25 +2,19 @@
 
 using i64 = long long;
 constexpr int INF = 1e9;
-
 template <typename T>
 struct Graph {
 	int n;
-	struct e { int from, to; T val; };
-	struct node {
-		int id;
-		T dis;
-		friend bool operator<(node a, node b) { return a.dis > b.dis; }
-	};
-	
+	struct e { int from, to; };
+
 	std::vector<std::vector<e>> g, rev, dag;
-	std::vector<int> val, f;
+	std::vector<int> val, f, val2;
 	std::vector<e> m;
 	
-	void add(int u, int v, T x) {
-		g[u].emplace_back(e{u, v, x});
-		rev[v].emplace_back(e{u, v, x});
-		m.emplace_back(e{u, v, x});
+	void add(int u, int v) {
+		g[u].emplace_back(e{u, v});
+		rev[v].emplace_back(e{v, u});
+		m.emplace_back(e{u, v});
 	}
 	
 	Graph(int n) {
@@ -28,11 +22,13 @@ struct Graph {
 		g.resize(n + 1);
 		rev.resize(n + 1);
 		val.resize(n + 1);
+		val2.resize(n + 1);
 		dag.resize(n + 1);
 		f.resize(n + 1);
 	}
 	
-	void init() {
+	void kosaraju() {
+		val2 = val;
 		std::stack<int> st;
 		std::vector<int> vis(n + 1);
 		auto dfs1 = [&](auto dfs1, int x) {
@@ -47,6 +43,7 @@ struct Graph {
 		auto dfs2 = [&](auto dfs2, int x, int y) {
 			if (!vis[x]) { return; }
 			f[x] = y;
+			val2[y] = std::max(val[x], val2[y]);
 			vis[x] = 0;
 			for (auto &i : g[x]) {
 				dfs2(dfs2, i.to, y);
@@ -62,12 +59,12 @@ struct Graph {
 		}
 		for (auto &i : m) {
 			if (f[i.from] != f[i.to]) {
-				dag[f[i.from]].emplace_back(e{f[i.from], f[i.to], i.val});
+				dag[f[i.from]].emplace_back(e{f[i.from], f[i.to]});
 			}
 		}
 	}
 	
-	i64 kosaraju() {
+	i64 count() {
 		std::unordered_map<int, int> cnt;
 		for (int i = 1; i <= n; ++i) { ++cnt[f[i]]; }
 		i64 ans = 0;
@@ -79,14 +76,19 @@ struct Graph {
 	
 	std::vector<T> query() {
 		std::vector<T> dp(n + 1);
-		auto dfs = [&](auto dfs, int x) -> T {
-			dp[x] = val[x];
+		auto dfs = [&](auto dfs, int x) {
+			if (dp[x]) return;
+			dp[x] = val2[x];
 			for (auto &i : dag[x]) {
-				dp[x] = std::max(dp[x], dfs(dfs, i.to));
+				dfs(dfs, i.to);
+				dp[x] = std::max(dp[x], dp[i.to]);
 			}
-			return dp[x];
 		};
-		dfs(dfs, 1);
+		for (int i = 1; i <= n; ++i) {
+			if (!dp[i]) {
+				dfs(dfs, i);
+			}
+		}
 		return dp;
 	}
 };
@@ -101,9 +103,9 @@ void solve() {
 	while (m--) {
 		int u, v;
 		std::cin >> u >> v;
-		g.add(u, v, 1);
+		g.add(u, v);
 	}
-	g.init();
+	g.kosaraju();
 	auto dp = g.query();
 	for (int i = 1; i <= n; ++i) {
 		std::cout << dp[i] << "\n";
